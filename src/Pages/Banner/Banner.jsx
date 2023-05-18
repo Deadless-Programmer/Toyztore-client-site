170a93514bd75caa426c882b896ba9b4a72bcd29
Deadless-Programmer <webtuhin@gmail.com>
2023-05-19 00:06:34 +0600

    banner section done

---
+++ src/Pages/Banner/Banner.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url("https://images.pexels.com/photos/3933025/pexels-photo-3933025.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold text-fuchsia-200">
+              "Bringing Smiles to Every Child's Face."
+            </h1>
+            <p className="mb-5 text-fuchsia-200">
+              Step into a world of wonder and excitement! Our toy website is a
+              treasure trove of imagination, offering a vast selection of toys
+              that spark joy and inspire endless adventures.
+            </p>
+            <button className="btn text-fuchsia-200 bg-fuchsia-600 border-0 hover:bg-cyan-600 ">
+              Shop now!{" "}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
