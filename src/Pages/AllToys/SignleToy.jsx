15285f0034f31a146715be5b2d0ec232c6f58270
Deadless-Programmer <webtuhin@gmail.com>
2023-05-20 23:57:57 +0600

    error fix

--- src/Pages/AllToys/SignleToy.jsx
+++ src/Pages/AllToys/SignleToy.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const SignleToy = ({ data }) => {
   console.log(data);
-  const { _id, sellerName, Price, Quantity,,SubCategory,toyName  } = data;
+  const { _id, sellerName, Price, Quantity,SubCategory,toyName  } = data;
   console.log(_id)
   return (
     <tr>
